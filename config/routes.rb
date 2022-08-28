8e30e4b23c68022bcb98ece5e8d691a400143b2d
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-08-28 12:54:52 +0900

    still in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :new, :show, :create] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+require "open-uri"
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+lemon_drip = Cocktail.create(name: "Lemon Drip")
+mint_mojito = Cocktail.create(name: "Mint Mojito")
+cocktail.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+cocktail.save
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
