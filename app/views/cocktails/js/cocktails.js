3e715495663c6a1ac37e75c94a41ae1ca3bacc11
Ayaka Inaba <ayakayakaaaa@gmail.com>
2022-09-12 16:49:59 +0900

    api

---
+++ app/views/cocktails/js/cocktails.js
@@ -0,0 +1,30 @@
+console.info('js/cocktails.js loaded');
+
+document.addEventListener('prechange', function(event){
+  document.querySelector('ons-toolerbar .center')
+    .innerHTML = event.tabItem.getAttribute('label');
+});
+
+function getRandomCocktail(){
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(
+    function(response) {
+        if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+        return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function(data) {
+        console.log(data);
+        });
+    }
+    )
+    .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+    });
+}
+
+
+getRandomCocktail();
